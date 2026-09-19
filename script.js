fddec55eacbb7a88637e69ef8d058bcf260530e8
Vijayaboopathi Easwaramoorthi <142922585+VijayaboopathiE@users.noreply.github.com>
2026-09-19 18:16:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+const bands = [
+    'The Plot in You',
+    'The Devil Wears Prada',
+    'Pierce the Veil',
+    'Norma Jean',
+    'The Bled',
+    'Say Anything',
+    'The Midway State',
+    'We Came as Romans',
+    'Counterparts',
+    'Oh, Sleeper',
+    'A Skylit Drive',
+    'Anywhere But Here',
+    'An Old Dog'
+];
+
+const bandList = document.getElementById('band');
+
+
+function removeArticle(band) {
+    return band.replace(/^(a |an |the )/i, '').trim();
+}
+bands.sort((a, b) => {
+    return removeArticle(a).localeCompare(removeArticle(b));
+});
+
+bands.forEach(band => {
+    const li = document.createElement('li');
+    li.textContent = band;
+    bandList.appendChild(li);
+});
